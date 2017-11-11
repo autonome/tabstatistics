2fb26b8763a2fc4200bc4a8aaf194b410b3e78ce
dietrich ayala <autonome@gmail.com>
2017-11-11 11:32:37 +0700

    loads of fixes

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # tabstatistics
-A few extensions for showing realtime stats for tab count, tabs opened, tabs closed and tab switches per session.
+
+A few extensions for showing real-time stats for tab count, tabs opened, tabs closed and tab switches per session.
+
+TODO
+
+* Re-enable storage debounce
+
+* Add popup that shows all stored data
+* Rotate between values?
+* Select visible badge?
+* Tooltip with all values?
+
+* Deterministically tell when session restore has ended, and start tracking tab events then
--- background.js
+++ background.js
@@ -1,18 +1,9 @@
 const STORAGE_KEY = 'data';
-const DISPLAY_KEY = browser.runtime.getManifest().displayKey;
+const STORAGE_KEY_DATE_PREFIX = 'date::';
+const STORAGE_DEBOUNCE_MS = 5000; // persist storage once per minute, max
+const DISPLAY_KEY = browser.runtime.getManifest().displayKey || 'tabsLastCount';
 
-let data = {};
-
-data[getDateKey()] = {
-  lastUpdated: (new Date()),
-  tabCounts: Array(24).fill(0),
-  tabsLastCount: 0,
-  tabsMaxCount: 0,
-  tabsMinCount: 0,
-  tabsOpened: 0,
-  tabsClosed: 0,
-  tabsSwitched: 0
-};
+let dateDataCache = null;
 
 let tabIds = [];
 
@@ -23,68 +14,169 @@ const prefixes = {
   tabsSwitched: '~' 
 };
 
-function getDateKey() {
-  const d = new Date();
-  return [d.getUTCDate(), d.getUTCMonth(), d.getUTCFullYear()].join('-');
-}
+// Listen for the reasons for activation, and pass
+// to initialization function.
+browser.runtime.onStartup.addListener(() => {
+  //console.log('onStartup')
+  init('startup');
+});
+browser.runtime.onInstalled.addListener(() => {
+  //console.log('onInstalled')
+  init('install');
+});
 
-(async function loadStorage() {
-  const storage = await browser.storage.local.get(STORAGE_KEY);
-
-  // Not first run!
-  //if (false){
-  if (storage.data) {
-    data = storage.data;
+async function init(reason) {
+  //console.log('init', reason, getDateKey());
+  
+  // TODO: This is so wrong. Figure out how to deterministically detect
+  // end of session restore.
+  if (reason == 'startup') {
+    //console.log('init: post-startup, so not listening to any tab events for 15s');
+    setTimeout(initEventListeners, 15000);
   }
-  // First run!
   else {
-    updateStorage();
+    initEventListeners();
   }
-  updateBadge();
-})();
 
-// Save changes to persistent storage
-function updateStorage() {
-  browser.storage.local.set({STORAGE_KEY, data});
+  // Populate UI
+  updateBadge();
 }
 
-function updateBadge() {
+// Save changes to persistent storage.
+// But only every STORAGE_DEBOUNCE_MS.
+let updateStorage = debounce(async function updateStorage() {
+  let dateKey = getDateKey();
+  let storageKey = STORAGE_KEY_DATE_PREFIX + dateKey;
+  let setParam = {};
+  setParam[storageKey] = dateDataCache;
+  await browser.storage.local.set(setParam);
+}, STORAGE_DEBOUNCE_MS);
+
+async function updateBadge() {
   const dateKey = getDateKey();
-  const str = prefixes[DISPLAY_KEY] + data[dateKey][DISPLAY_KEY].toString();
+  const dateData = await getDateData(dateKey);
+  const str = prefixes[DISPLAY_KEY] + dateData[DISPLAY_KEY].toString();
   browser.browserAction.setBadgeText({text: str});
 }
 
 async function updateData(eventType) {
   const dateKey = getDateKey();
+  let dateData = await getDateData(dateKey);
   if (eventType == 'onActivated') {
-    data[dateKey].tabsSwitched++;
+    dateData.tabsSwitched++;
   }
   else {
     if (eventType == 'onCreated') {
-      data[dateKey].tabsOpened++;
+      dateData.tabsOpened++;
+      dateData.tabsSwitched++;
     }
     else if (eventType == 'onRemoved') {
-      data[dateKey].tabsClosed++;
+      dateData.tabsClosed++;
     }
 
     let numTabs = await getTabCount();
     if (eventType == 'onRemoved') {
       numTabs--;
     }
-    data[dateKey].tabsLastCount = numTabs;
-    data[dateKey].tabCounts[ (new Date()).getUTCHours() ] = numTabs;
+    dateData.tabsLastCount = numTabs;
+    dateData.tabCounts[ (new Date()).getUTCHours() ] = numTabs;
 
-    if (numTabs > data[dateKey].tabsMaxCount) {
-      data[dateKey].tabsMaxCount = numTabs;
+    if (numTabs > dateData.tabsMaxCount) {
+      dateData.tabsMaxCount = numTabs;
     }
 
-    if (data[dateKey].tabsMinCount === 0 || numTabs < data[dateKey].tabsMinCount) {
-      data[dateKey].tabsMinCount = numTabs;
+    if (dateData.tabsMinCount === 0 || numTabs < dateData.tabsMinCount) {
+      dateData.tabsMinCount = numTabs;
     }
   }
   updateBadge();
-  updateStorage();
+  await setDateData(dateKey, dateData);
 }
+
+function getDateKey() {
+  const d = new Date();
+  return [d.getUTCDate(), d.getUTCMonth(), d.getUTCFullYear()].join('-');
+}
+
+// Initializes persistent storage and cache if doesn't exist.
+async function getDateData(dateKey) {
+  // If no cache (eg, we just started up)...
+  let dateData = getDateDataCache();
+  // Or date changed while we've been running...
+  if (!dateData || dateData.dateKey != dateKey) {
+    // Get data from storage.
+    const storageKey = STORAGE_KEY_DATE_PREFIX + dateKey;
+    let items = await browser.storage.local.get(storageKey);
+    // If no storage record for this date...
+    if (!items[storageKey]) {
+      // initialize date storage for the date.
+      dateData = {
+        dateKey: dateKey,
+        lastUpdated: (new Date()),
+        tabCounts: Array(24).fill(0),
+        tabsLastCount: 0,
+        tabsMaxCount: 0,
+        tabsMinCount: 0,
+        tabsOpened: 0,
+        tabsClosed: 0,
+        tabsSwitched: 0
+      };
+      // Store it.
+      await setDateData(dateKey, dateData);
+    }
+    else {
+      dateData = items[storageKey];
+    }
+    // Update cache
+    setDateDataCache(dateData);
+  }
+
+  return dateData;
+}
+
+// These are the only two bits touching that one global.
+function getDateDataCache() {
+  return dateDataCache;
+}
+
+function setDateDataCache(dateData) {
+  dateDataCache = dateData;
+}
+
+async function setDateData(dateKey, dateData) {
+  setDateDataCache(dateData);
+  await updateStorage();
+}
+
+function initEventListeners() {
+  getTabCount();
+
+  browser.tabs.onCreated.addListener(tab => {
+    //console.log('onCreated');
+    updateData('onCreated');
+  });
+
+  browser.tabs.onRemoved.addListener(tab => {
+    //console.log('onRemoved');
+    updateData('onRemoved');
+    evictTabIdFromCache(tab.id);
+  });
+
+  browser.tabs.onActivated.addListener(info => {
+    //console.log('onActivated');
+    if (!isNewTabId(info.tabId)) {
+      updateData('onActivated');
+    }
+  });
+}
+
+/*
+
+Tab Cache: Manually create and manage a cache of ids of current tabs
+because the core APIs can't do things like let you know if a user
+switched between two different tabs.
+
+*/
 
 async function getTabCount() {
   const tabs = await browser.tabs.query({});
@@ -103,32 +195,25 @@ function isNewTabId(id) {
   }
 }
 
-function evictTabId(id) {
+function evictTabIdFromCache(id) {
   const index = tabIds.indexOf(id);
   if (index != -1) {
     tabIds.splice(index, 1);
   }
 }
 
-browser.tabs.onCreated.addListener(tab => {
-  updateData('onCreated');
-});
-
-browser.tabs.onRemoved.addListener(tab => {
-  updateData('onRemoved');
-  evictTabId(tab.id);
-});
-
-browser.tabs.onActivated.addListener(info => {
-  if (!isNewTabId(info.tabId)) {
-    updateData('onActivated');
-  }
-});
-
-if (['tabsLastCount', 'tabsSwitched'].indexOf(DISPLAY_KEY) > -1) {
-  updateData('fluglehorn');
-}
-else {
-  updateBadge();
-}
-
+// https://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
